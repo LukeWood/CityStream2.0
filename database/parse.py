9e99ca4e012aa75c2fd26d44ee0efc04313538d0
Lucas Wood <lucaswood@Lucass-MacBook-Pro.local>
2016-12-22 14:20:25 -0800

    parse.py starting to work a bit"

--- database/parse.py
+++ database/parse.py
@@ -1,1 +1,15 @@
-import pandas
+lines = [x.split('\t') for x in open("raw/rawdb.tsv")]
+mapping = {}
+lines = [x[1:13] for x in lines]
+for i,field in enumerate(lines[0]):
+    mapping[i] = field
+
+
+xps = []
+for line in lines[1:]:
+    d = {}
+    for i, data in enumerate(line):
+        d[mapping[i]] = data
+    xps.append(d)
+
+print(xps[1])
