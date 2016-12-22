2f4c247ed3d794c17de08b0fd86adb57ee2b50e9
Lucas Wood <lucaswood@Lucass-MacBook-Pro.local>
2016-12-22 14:36:39 -0800

    graph construction

--- database/parse.py
+++ database/parse.py
@@ -1,6 +1,9 @@
+import json
+
 lines = [x.split('\t') for x in open("raw/rawdb.tsv")]
 mapping = {}
-lines = [x[1:13] for x in lines]
+lines = [x[1:20] for x in lines]
+
 for i,field in enumerate(lines[0]):
     mapping[i] = field
 
@@ -12,4 +15,22 @@ for line in lines[1:]:
         d[mapping[i]] = data
     xps.append(d)
 
-print(xps[1])
+xps = [x for x in xps if "Tags" in x and x["Tags"] != ""]
+
+for x in xps:
+    x["Tags"] = [z.strip() for z in x["Tags"].lower().split(",")]
+
+sets = {}
+
+for x in xps:
+    for tag in x["Tags"]:
+        if(tag.replace(" ","_") not in sets):
+            sets[tag.replace(" ","_")] = []
+        sets[tag.replace(" ","_")].append(x["ID"])
+
+print(sets)
+
+with open("graphdata/dump.json","w") as f:
+    f.write(json.dumps(sets));
+
+#next insert into a mongodb file
