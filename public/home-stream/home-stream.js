24c57a03e25ff3ede9b109c6b54a3320559fb787
Lucas Wood <lucaswood@Lucass-MacBook-Pro.local>
2016-12-22 13:31:05 -0800

    a

--- public/home-stream/home-stream.js
+++ public/home-stream/home-stream.js
@@ -1,4 +1,16 @@
 //forcing
+var slideout = new Slideout({
+	'panel': document.getElementById('panel'),
+	'menu': document.getElementById('menu'),
+	'padding': 256,
+	'tolerance': 70
+});
+
+// Toggle button
+document.querySelector('.toggle-button').addEventListener('click', function() {
+	slideout.toggle();
+});
+
 Vue.component("square",{
 	props: ["event"],
 	template:
