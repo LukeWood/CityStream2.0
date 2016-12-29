31f95e95141da816be72da9a4ab20d3ffd3328e6
Lucas Wood <lucaswood@Lucass-MacBook-Pro.local>
2016-12-28 19:10:45 -0800

    a

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const session = require("express-session");
 const tag_routing = require("./graph/tag_routing.js");
 const gen_uid = require("./util/gen_uid.js");
+const path = require('path');
+
 
 app.use(session({
   secret: 'keyboard cat',
@@ -17,6 +19,20 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/next_xp",tag_routing.next_xp);
 app.get("/next_tag",tag_routing.next_tag);
+app.get("/img_exist",function(req,res){
+  if(req.query.fname == null){
+    res.send(JSON.stringify({success:false}));
+    return;
+  }
+  path.exists(req.query.fname, function(exists) {
+    if (exists) {
+      res.send(JSON.stringify({success:true}));
+    }
+    else{
+      res.send(JSON.stringify({success:false}));
+    }
+  });
+});
 
 app.post("/clicked",tag_routing.clicked);
 
